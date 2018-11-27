c957c0846dcb5c34c6b5194d972afbd49cd67555
Real Lost <thingone656@gmail.com>
2018-11-27 09:15:02 -0500

    updated gh-pages

---
+++ precache-manifest.e28238bbadd30ba3f566dc601d75a086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a2f772776f0dc55f02",
+    "url": "/bfv/static/js/runtime~main.43a2f772.js"
+  },
+  {
+    "revision": "5cd5967f77e62531531a",
+    "url": "/bfv/static/js/main.5cd5967f.chunk.js"
+  },
+  {
+    "revision": "3e7337394c412f63cc84",
+    "url": "/bfv/static/js/1.3e733739.chunk.js"
+  },
+  {
+    "revision": "5cd5967f77e62531531a",
+    "url": "/bfv/static/css/main.7a288fd1.chunk.css"
+  },
+  {
+    "revision": "3e7337394c412f63cc84",
+    "url": "/bfv/static/css/1.aa6dc50a.chunk.css"
+  },
+  {
+    "revision": "780a467d021329a2a5d3d7b7bbe02836",
+    "url": "/bfv/index.html"
+  }
+];
